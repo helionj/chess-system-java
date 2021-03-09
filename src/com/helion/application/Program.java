4bbe3c2b43f7c48128f208eac0bb513e51f1c969
helionporto <helionporto@hotmail.com>
2021-03-09 08:57:23 -0400

    First class Position

--- src/com/helion/application/Program.java
+++ src/com/helion/application/Program.java
@@ -1,10 +1,13 @@
 package com.helion.application;
 
+import com.helion.boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		Position position = new Position(5,10);
+		System.out.println(position);
 	}
 
 }
