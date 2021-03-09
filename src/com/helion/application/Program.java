7aacd6259c461677ed99dc1700fe32b212c1991e
helionporto <helionporto@hotmail.com>
2021-03-09 09:38:41 -0400

    Starting to implement Board and Piece

--- src/com/helion/application/Program.java
+++ src/com/helion/application/Program.java
@@ -1,13 +1,12 @@
 package com.helion.application;
 
-import com.helion.boardgame.Position;
+import com.helion.boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(5,10);
-		System.out.println(position);
+		Board board = new Board(8,8);
 	}
 
 }
---
+++ src/com/helion/boardgame/Piece.java
@@ -0,0 +1,5 @@
+package com.helion.boardgame;
+
+public class Piece {
+
+}
