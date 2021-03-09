7cd41fe9d786a3e31e4a0dd34243c3b69f0e0e3d
helionporto <helionporto@hotmail.com>
2021-03-09 16:15:35 -0400

    BoardException and defensive programming

---
+++ src/com/helion/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package com.helion.boardgame;
+
+public class BoardException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+
+}
