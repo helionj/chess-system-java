d51504f0dc7a504a795545209dc2b081663ba34b
helionporto <helionporto@hotmail.com>
2021-03-11 17:06:14 -0400

    Handling cptured pieces

--- src/com/helion/chess/ChessMatch.java
+++ src/com/helion/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package com.helion.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.helion.boardgame.Board;
 import com.helion.boardgame.Piece;
 import com.helion.boardgame.Position;
@@ -11,7 +14,8 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
-	
+	List<Piece> piecesOnTheBoard = new ArrayList<>();
+	List<Piece> capturedPieces = new ArrayList<>();
 
 	
 	public ChessMatch() {
@@ -63,6 +67,10 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -91,6 +99,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	private void initialSetup() {
