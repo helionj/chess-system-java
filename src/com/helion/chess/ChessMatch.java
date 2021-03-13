29001546c6163cbe6c5638ed4feaf61b91a56c20
helionporto <helionporto@hotmail.com>
2021-03-13 08:36:51 -0400

    Check logic

--- src/com/helion/chess/ChessMatch.java
+++ src/com/helion/chess/ChessMatch.java
@@ -2,6 +2,8 @@ package com.helion.chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.helion.boardgame.Board;
 import com.helion.boardgame.Piece;
@@ -14,6 +16,8 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	private boolean check;
+	
 	List<Piece> piecesOnTheBoard = new ArrayList<>();
 	List<Piece> capturedPieces = new ArrayList<>();
 
@@ -33,6 +37,11 @@ public class ChessMatch {
 	public Color getCurrentPlayer() {
 		return currentPlayer;
 	}
+	
+	public boolean getCheck() {
+		return check;
+	}
+	
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		
@@ -52,6 +61,12 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		if(testCheck(currentPlayer)) {
+			undoMove(source,target, capturedPiece);
+			throw new ChessException("You cant put yourself in check");
+		}
+		
+		check =(testCheck(opponent(currentPlayer))) ? true : false;
 		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
@@ -71,7 +86,19 @@ public class ChessMatch {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
+		
 		return capturedPiece;
+	}
+	
+	private void undoMove(Position source, Position target, Piece capturedPiece) {
+		Piece p = board.removePiece(target);
+		board.placePiece(p, source);
+		
+		if(capturedPiece != null) {
+			board.placePiece(capturedPiece, target);
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+		}
 	}
 	
 	private void validateTargetPosition(Position source, Position target) {
@@ -100,6 +127,36 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 		piecesOnTheBoard.add(piece);
+	}
+	
+	private Color opponent(Color color) {
+		return (color == color.WHITE) ? color.BLACK : color.WHITE;
+	}
+	
+	private ChessPiece king(Color color) {
+		List<Piece> list = piecesOnTheBoard
+				.stream().filter(x -> ((ChessPiece)x).getColor()==color).collect(Collectors.toList());
+		
+		for(Piece p: list) {
+			if(p instanceof King) {
+				return (ChessPiece)p;
+			}
+		}
+		throw new IllegalStateException("There is no "+color+ " king on the board");
+	}
+	
+	private boolean testCheck(Color color) {
+		Position kingPosition = king(color).getChessPosition().toPosition();
+		List<Piece> list = piecesOnTheBoard
+				.stream().filter(x -> ((ChessPiece)x).getColor()==opponent(color)).collect(Collectors.toList());
+		
+		for(Piece p: list) {
+			boolean [][]mat = p.possibleMoves();
+			if(mat[kingPosition.getRow()][kingPosition.getColumn()]){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	private void initialSetup() {
--- src/com/helion/chess/ChessPiece.java
+++ src/com/helion/chess/ChessPiece.java
@@ -17,12 +17,22 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	//public ChessPosition getChessPiece() {
+	//	return ChessPosition.fromPosition(position);
+	//}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 		
 	}
+	
+	
 
 	
 
